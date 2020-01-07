35318a73f763aa6d44c7e2b32956b520475fd5bd
wesaspinall <wes@wesaspinall.com>
2020-01-06 20:48:46 -0500

    https://www.codewars.com/kata/create-phone-number/

---
+++ codewars/create_phone_number.go
@@ -0,0 +1,15 @@
+package codewars
+
+import "fmt"
+
+//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+//
+//Example:
+//CreatePhoneNumber([10]uint{1,2,3,4,5,6,7,8,9,0})  // returns "(123) 456-7890"
+//The returned format must be correct in order to complete this challenge.
+//Don't forget the space after the closing parentheses!
+
+func CreatePhoneNumber(a [10]uint) string {
+
+	return fmt.Sprintf("(%v%v%v) %v%v%v-%v%v%v%v", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9])
+}
